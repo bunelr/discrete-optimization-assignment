70aa8d8eb8d67e6b8fcdeecf6d5d1f4a63a8c18d
Rudy Bunel <bunel.rudy@gmail.com>
2015-03-05 15:48:48 +0100

    Unzipping of the submission

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
 from flaskext.uploads import configure_uploads, patch_request_class, UploadSet, UploadNotAllowed
 from flask.ext.basicauth import BasicAuth
+import shutil
+import os
+import zipfile
 
 from werkzeug import secure_filename
 
+LOCAL_ARCHIVE_NAME = 'code.zip'
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -23,18 +27,35 @@ def upload():
        ('username' in request.form) and \
        (request.files['code'].filename != '') and \
        request.form['username']!= '':
+
+
         try:
             username = request.form['username']
             clean_path = secure_filename(username)
-            filename = archives.save(request.files['code'], folder=clean_path, name='code.zip')
+            filename = archives.save(request.files['code'], folder=clean_path, name=LOCAL_ARCHIVE_NAME)
         except UploadNotAllowed:
             return render_template('fuck_up.html', reason="Problem with the upload")
+
+        user_folder = app.config['SUBMISSION_FOLDER']
+
+        work_directory = unzip(user_folder, filename)
+
 
         return render_template('ok.html')
     elif(request.method == 'GET'):
         return render_template('submit.html')
     else:
         return render_template('fuck_up.html', reason='Your submission is missing something')
+
+
+def unzip(folder, filename):
+    zip_file = os.path.join(folder, filename)
+    user_dir = os.path.dirname(zip_file)
+    with zipfile.ZipFile(zip_file, "r") as z:
+        z.extractall(user_dir)
+    content = os.listdir(user_dir)
+    container = [name for name in content if not name.endswith('.zip')][0]
+    return os.path.join(user_dir, container)
 
 if __name__ == '__main__':
     app.run(debug=True)
