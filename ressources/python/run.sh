e52d1b8ef28632e55114ee199a6da9057720674b
Rudy Bunel <bunel.rudy@gmail.com>
2015-03-05 18:28:15 +0100

    Requirements.txt and pyton fixup

--- ressources/python/run.sh
+++ ressources/python/run.sh
@@ -2,16 +2,17 @@
 
 if [ "$1" = 1 ]; then
 
-        if ! python -c "from main import run;run(1)"; then
-                exit 4
-        fi
+    if ! python -c "from main import run;run(1)"; then
+        exit 4
+    fi
 
-        if ! ./testResults.py; then
-                exit 3
+    if ! ./testResults.py; then
+        exit 3
+    fi
 fi
 
 if ! python -c "from main import run;run(0)"; then
-        exit 5
+    exit 5
 fi
 
 exit 0
