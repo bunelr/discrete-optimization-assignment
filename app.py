2987b2a981fbc7b9237150f4c31b770631c51780
Rudy Bunel <bunel.rudy@gmail.com>
2015-03-05 14:47:27 +0100

    Allow file uploads

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flaskext.uploads import configure_uploads, patch_request_class, UploadSet, UploadNotAllowed
+from werkzeug import secure_filename
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+
+archives = UploadSet(extensions=['zip'], default_dest = lambda app: app.config['SUBMISSION_FOLDER'])
+
+configure_uploads(app, archives)
+patch_request_class(app, size = 5*1024*1024)
+
+@app.route('/submit', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'code' in request.files:
+        try:
+            username = request.form['username']
+            clean_path = secure_filename(username)
+            filename = archives.save(request.files['code'], folder=clean_path, name='code.zip')
+        except UploadNotAllowed:
+            return render_template('fuck_up.html', reason="Problem with the upload")
+
+
+        return render_template('ok.html')
+    return render_template('submit.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,2 @@
+class Config(object):
+    SUBMISSION_FOLDER = './submission'
