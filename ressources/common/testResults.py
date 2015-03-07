ddabc7cb2d468042357e7725708c3a4c812dda94
Rudy Bunel <bunel.rudy@gmail.com>
2015-03-07 15:08:42 +0100

    More robust result verification

--- ressources/common/testResults.py
+++ ressources/common/testResults.py
@@ -4,20 +4,20 @@ import os
 import sys
 
 groundTruth = []
-for line in  open("groundTruth.txt").read().split("\n")[:-1]:
+for line in  open("groundTruth.txt").read().strip().split("\n"):
     groundTruth.append([float(val) for val in line.split()])
 
 fwresult = []
-for line in  open("fw.txt").read().split("\n")[:-1]:
+for line in  open("fw.txt").read().strip().split("\n"):
     fwresult.append([float(val) for val in line.split()])
 
 bfresult = []
-for i in range(0,304):
-    bfresult.append([float(val) for val in open("bf"+str(i)+".txt").read().split("\n")[-2].split()])
+for i in range(304):
+    bfresult.append([float(val) for val in open("bf"+str(i)+".txt").read().strip().split("\n")[-1].split()])
 
 dijresult = []
-for i in range(0, 304):
-    dijresult.append([float(val) for val in open("dijkstra"+str(i)+".txt").read().split("\n")[-2].split() ])
+for i in range(304):
+    dijresult.append([float(val) for val in open("dijkstra"+str(i)+".txt").read().strip().split("\n")[-1].split() ])
 
 if not (bfresult == groundTruth):
     print "[ERROR]: Your Bellman-Ford results are incorrect"
